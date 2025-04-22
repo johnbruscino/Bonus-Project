61db4498a338fa7d998c2b0ab98695d67717af1b
johnbruscino <johnbruscino2@gmail.com>
2025-04-22 19:23:07 -0400

    Done

---
+++ bonusMain.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int numbers[] = {1, 2, 3, 4, 5}; // Example array
+    int sum = 0;
+    int length = sizeof(numbers) / sizeof(numbers[0]);
+
+    for (int i = 0; i < length; i++) {
+        sum += numbers[i];
+    }
+
+    printf("The sum of the array is: %d\n", sum);
+
+    return 0;
+}
